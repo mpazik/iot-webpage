25b41de9b16322ee3caaac9005ae17e7142ddc6d
Marek Pazik <poczta@marekpazik.pl>
2016-09-03 13:47:10 +0200

    Ability to add custom scripts.

--- layout-fragments/main.js
+++ layout-fragments/main.js
@@ -1,11 +1,17 @@
 const nav = require('./navigation');
 
-module.exports = (data) =>`
+module.exports = (data) => {
+    var scripts = '';
+    if (data.scripts) {
+        scripts = data.scripts.map(script => `<script src="${script}"></script>`).join('/n');
+    }
+    return `
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="/theme.css">
         <link rel="stylesheet" type="text/css" href="/style.css">
         <title>${data.title}</title>
+        ${scripts}
     </head>
     <body>
         ${nav()}
@@ -14,4 +20,5 @@ module.exports = (data) =>`
         </main>
     </body>
 </html>
-`;
+`
+};
