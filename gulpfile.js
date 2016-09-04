8dae23df38bc3b6324851b2a5cdf6f82cb97b000
Marek Pazik <poczta@marekpazik.pl>
2016-09-04 18:09:28 +0200

    Publishing page do aws.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,20 @@ const renderJs = require('./gulp-render-js');
 const del = require('del');
 const lastRun = require('last-run');
 const webserver = require('gulp-webserver');
+const AwsPublish = require('gulp-awspublish');
+
+const path = {
+    pages: './pages/*.md',
+    layout: './layout',
+    layoutFiles: './layout/**/*',
+    layoutFragmentFiles: './layout-fragments/**/*',
+    publicFiles: './public/**/*',
+    build: './build',
+    buildFiles: './build/**/*',
+    prodFiles: './prod/**/*',
+    dist: './dist',
+    distFiles: './dist/**/*'
+};
 
 gulp.task('build', gulp.series(buildPages, copyResources));
 
@@ -13,44 +27,47 @@ gulp.task('build:clean', gulp.series(clean, 'build'));
 
 gulp.task('default', gulp.series('build', gulp.parallel(watchPages, watchLayout, watchResources, runWebserver)));
 
+gulp.task('build:dist', gulp.series(cleanDist, 'build:clean', copyToDist, overrideByProdFiles));
+
+gulp.task('publish', gulp.series(publishToAws));
 
 function clean() {
-    return del('build/**/*');
+    return del(path.build);
 }
 
 function buildPages() {
-    return gulp.src('./pages/*.md', {since: gulp.lastRun(buildPages)})
+    return gulp.src(path.pages, {since: gulp.lastRun(buildPages)})
         .pipe(frontMatter({
             property: 'meta', // property added to file object
             remove: true // should we remove front-matter header?
         }))
         .pipe(marked())
         .pipe(renderJs({
-            layoutDir: './layout'
+            layoutDir: path.layout
         }))
         .pipe(rename((path) => {
             path.dirname += '/' + path.basename;
             path.basename = 'index';
             return path;
         }))
-        .pipe(gulp.dest('./build'))
+        .pipe(gulp.dest(path.build))
 }
 
 function copyResources() {
-    return gulp.src('./public/**/*', {since: gulp.lastRun('build')})
-        .pipe(gulp.dest('./build'))
+    return gulp.src(path.publicFiles, {since: gulp.lastRun('build')})
+        .pipe(gulp.dest(path.build))
 }
 
 function watchPages() {
-    return gulp.watch(['./pages/*.md'], gulp.parallel(buildPages));
+    return gulp.watch([path.pages], gulp.parallel(buildPages));
 }
 
 function watchResources() {
-    return gulp.watch(['./public/**/*'], gulp.parallel(copyResources));
+    return gulp.watch([path.publicFiles], gulp.parallel(copyResources));
 }
 
 function watchLayout() {
-    return gulp.watch(['./layout/**/*', './layout-fragments/**/*'], gulp.parallel(buildPages))
+    return gulp.watch([path.layoutFiles, path.layoutFragmentFiles], gulp.parallel(buildPages))
         .on('change', () => {
             lastRun.release(buildPages);
         });
@@ -61,4 +78,40 @@ function runWebserver() {
         .pipe(webserver({
             livereload: false
         }));
+}
+
+function cleanDist() {
+    return del(path.dist);
+}
+
+function copyToDist() {
+    return gulp.src(path.buildFiles)
+        .pipe(gulp.dest(path.dist));
+}
+
+function overrideByProdFiles() {
+    return gulp.src(path.prodFiles)
+        .pipe(gulp.dest(path.dist));
+}
+
+function publishToAws() {
+    process.env['AWS_PROFILE'] = 's3_deploy';
+
+    const publisher = AwsPublish.create({
+        region: 'eu-central-1',
+        params: {
+            Bucket: 'islesoftales.com'
+        }
+    });
+
+    var headers = {
+        // Enable cache when files will be versioned, for now it's only symbolic minute
+        'Cache-Control': 'max-age=60, no-transform, public'
+    };
+
+    return gulp.src(path.distFiles)
+        .pipe(AwsPublish.gzip())
+        .pipe(publisher.publish(headers))
+        .pipe(publisher.cache())
+        .pipe(AwsPublish.reporter());
 }
---
+++ prod/src/configuration.js
@@ -0,0 +1,5 @@
+Configuration = {
+    gameSiteStorageUrl: 'http://game.islesoftales.com/crossSiteStorage.html',
+    userServiceUrl: 'http://game.islesoftales.com:7000',
+    gameUrl: 'http://game.islesoftales.com',
+};
