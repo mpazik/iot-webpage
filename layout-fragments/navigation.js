d3dd0615fd5264635e71ecac0ee7f5f34a7aa133
Marek Pazik <poczta@marekpazik.pl>
2016-09-04 18:59:34 +0200

    Fixed email address.

--- layout-fragments/navigation.js
+++ layout-fragments/navigation.js
@@ -3,7 +3,7 @@ module.exports = () =>`
     <a href="/registration">Registration</a>
     <a href="/about-game">About game</a>
     <a href="/news">News</a>
-    <a href="mailto:contact@ilesoftales.com">Contact</a>
+    <a href="mailto:contact@islesoftales.com">Contact</a>
     <button id="login-button">Login</a>
 </nav>
 `;
